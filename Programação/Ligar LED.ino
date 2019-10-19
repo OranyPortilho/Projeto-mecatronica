3933430068b0100c6967d627c39693c345dd313a
OranyPortilho <49995251+OranyPortilho@users.noreply.github.com>
2019-10-19 16:26:48 -0300

    Add files via upload

---
+++ Programação/Ligar LED.ino
@@ -0,0 +1,49 @@
+  void setup()
+  {
+     pinMode ( 10,OUTPUT);
+     pinMode ( 7, OUTPUT);
+     pinMode ( 5, OUTPUT);
+     pinMode ( 4, OUTPUT);
+     pinMode ( 3, OUTPUT);
+  }
+  void loop()
+  {
+          //Porta digital 10
+         
+        digitalWrite(10, 1);
+        delay (100);
+        digitalWrite (10, 0);
+        delay (200);
+        
+          //Porta digital 7
+          
+        digitalWrite(7, 1);
+        delay (100);
+        digitalWrite (7, 0);
+        delay (200);
+
+          //Porta digital 5
+          
+        digitalWrite(5, 1);
+        delay (100);
+        digitalWrite (5, 0);
+        delay (200);
+
+          //Porta digital 4
+          
+        digitalWrite(4, 1);
+        delay (100);
+        digitalWrite (4, 0);
+        delay (200);
+
+          //Porta digital 3
+          
+        digitalWrite(3, 1);
+        delay (100);
+        digitalWrite (3, 0);
+        delay (200);
+        
+  }
+  
+  
+     
